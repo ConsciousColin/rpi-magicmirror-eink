9e082e25a334210729e7852caf0c34bf6bd306f8
Colin <ckrogers201@gmail.com>
2021-04-02 11:33:07 -0700

    Uncomment frame display

--- ePaperPython/main.py
+++ ePaperPython/main.py
@@ -28,7 +28,7 @@ import epd7in5
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-#import imagedata
+import imagedata
 
 EPD_WIDTH = 640
 EPD_HEIGHT = 384
@@ -38,22 +38,22 @@ def main():
     epd.init()
 
     # For simplicity, the arguments are explicit numerical coordinates
-    # image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
-    # draw = ImageDraw.Draw(image)
-    # font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
-    # draw.rectangle((0, 6, 640, 30), fill = 0)
-    # draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
-    # draw.rectangle((200, 80, 600, 280), fill = 0)
-    # draw.arc((240, 120, 580, 220), 0, 360, fill = 255)
-    # draw.rectangle((0, 80, 160, 280), fill = 255)
-    # draw.arc((40, 80, 180, 220), 0, 360, fill = 0)
-    # epd.display_frame(epd.get_frame_buffer(image))
+    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
+    draw.rectangle((0, 6, 640, 30), fill = 0)
+    draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
+    draw.rectangle((200, 80, 600, 280), fill = 0)
+    draw.arc((240, 120, 580, 220), 0, 360, fill = 255)
+    draw.rectangle((0, 80, 160, 280), fill = 255)
+    draw.arc((40, 80, 180, 220), 0, 360, fill = 0)
+    epd.display_frame(epd.get_frame_buffer(image))
 
     image = Image.open('screenshot.png')
     epd.display_frame(epd.get_frame_buffer(image))
 
     # You can get frame buffer from an image or import the buffer directly:
-    #epd.display_frame(imagedata.MONOCOLOR_BITMAP)
+    epd.display_frame(imagedata.MONOCOLOR_BITMAP)
 
 if __name__ == '__main__':
     main()
